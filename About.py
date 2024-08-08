4e69ec2ef0db931036f21cfb8a363d72855f1fd5
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-08-08 12:13:51 -0600

    Solana

--- About.py
+++ About.py
@@ -17,29 +17,29 @@ st.write('Copyright © 2023-2024, SMARTEST - El equipo de desarrollo del Sistema
 st.write('')
 
 
-st.subheader('LICENCIA')
+st.markdown('<p style="display: block;  font-size: 25px;  font-weight:bold; color: #000;">LICENCIA</p>', unsafe_allow_html=True)
 st.write('''El uso de productos de software o hardware de SMARTEST se basa en la licencia de software y ciertos términos y condiciones para el producto al momento de su compra. La aceptación de esos términos es obligatoria para instalar o usar el producto. Consulta la licencia con tu proveedor o comunícate con nuestra área comercial ventas@smartest.mx.''')
 st.write('')
 
 
-st.subheader('MARCAS')
+st.markdown('<p style="display: block;  font-size: 25px;  font-weight:bold; color: #000;">MARCAS</p>', unsafe_allow_html=True)
 st.write('''El logotipo del Sistema de Soldadura y de SMARTEST son marcas registradas o marcas comerciales de SMARTEST en México.''')
 st.write('')
 
 
 
-st.subheader('POLÍTICAS Y AVISOS')
+st.markdown('<p style="display: block;  font-size: 25px;  font-weight:bold; color: #000;">POLÍTICAS Y AVISOS</p>', unsafe_allow_html=True)
 st.write('''Los avisos, términos y condiciones pertenecientes a software, hardware o datos de terceros se encuentran en https://www.smartest.mx/.''')
 st.write('')
 
 
-st.subheader('DERECHOS Y PERMISOS')
+st.markdown('<p style="display: block;  font-size: 25px;  font-weight:bold; color: #000;">DERECHOS Y PERMISOS</p>', unsafe_allow_html=True)
 st.write('''La instalación y las adecuaciones del Sistema de Soldadura están limitadas a acciones de los técnicos de SMARTEST.''')
 st.write('')
 
 
-st.subheader('INTELIGENCIA ARTIFICIAL')
+st.markdown('<p style="display: block;  font-size: 25px;  font-weight:bold; color: #000;">INTELIGENCIA ARTIFICIAL.</p>', unsafe_allow_html=True)
 st.write('''El producto Sistema de Soldadura tiene inteligencia artificial ("IA"). Este es desarrollado por SMARTEST. La funcionalidad de la IA esta especificada en las características del producto o servicio. Las funciones de IA utilizan la información que el sistema recopila para mejorar los servicios que te proporcionamos o para mejorar la asistencia que te ofrecemos en relación con las operaciones del sistema.''')
 
 st.write('')
-st.subheader('El Sistema de Soldadura es creado por los SMARTEST.')
+st.markdown('<p style="display: block; text-align:center; font-size: 22px;  font-weight:bold;  text-align:center;color: #000;">El Sistema de Soldadura es creado por los SMARTEST.</p>', unsafe_allow_html=True)
