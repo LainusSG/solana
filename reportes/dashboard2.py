6e6e5c3fef914c239818cc4720d8a84343b67c2a
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-08-12 12:26:32 -0600

    Solana

--- reportes/dashboard2.py
+++ reportes/dashboard2.py
@@ -220,7 +220,7 @@ with col1:
     filtered_df["Año"] = filtered_df["FECHA"].dt.to_period("Y")
     st.write('<p style="font-size:25px; font-weight:bold; text-align:center;"> Fallas por Año</p>', unsafe_allow_html=True)
 
-    linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Año"].dt.strftime("%Y : %b"))["FALLAS"].sum()).reset_index()
+    linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Año"].dt.strftime("%Y"))["FALLAS"].sum()).reset_index()
     fig5 = px.line(linechart, x = "Año", y="FALLAS", labels = {"FALLAS": "Cantidad"},height=500, width = 1000,template="ggplot2")
     fig5.update_layout({
     'plot_bgcolor': 'rgba(0, 0, 0, 0)',
@@ -242,7 +242,7 @@ with col2:
     filtered_df["Mes"] = filtered_df["FECHA"].dt.to_period("M")
     st.write('<p style="font-size:25px; font-weight:bold; text-align:center;"> Fallas por Mes</p>', unsafe_allow_html=True)
 
-    linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Mes"].dt.strftime("%Y : %b"))["FALLAS"].sum()).reset_index()
+    linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Mes"].dt.strftime("%B"))["FALLAS"].sum()).reset_index()
     fig6 = px.line(linechart, x = "Mes", y="FALLAS", labels = {"FALLAS": "Cantidad"},height=500, width = 1000,template="ggplot2")
     fig6.update_layout({
     'plot_bgcolor': 'rgba(0, 0, 0, 0)',
@@ -259,6 +259,52 @@ with col2:
 
 
 ########################################################################################   
+
+
+col1, col2 = st.columns((2))
+
+########################################################################################           
+with col1:
+    filtered_df["Semana"] = filtered_df["FECHA"].dt.to_period("W")
+    st.write('<p style="font-size:25px; font-weight:bold; text-align:center;"> Fallas por Semana</p>', unsafe_allow_html=True)
+
+    linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Semana"].dt.strftime("%W"))["FALLAS"].sum()).reset_index()
+    fig9 = px.line(linechart, x = "Semana", y="FALLAS", labels = {"FALLAS": "Cantidad"},height=500, width = 1000,template="ggplot2")
+    fig9.update_layout({
+    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+    fig9.update_traces(marker_color='#FF8000')
+    st.plotly_chart(fig9,use_container_width=True)
+
+    with st.expander("Fallas Por Semana"):
+        st.write(linechart.T.style.background_gradient(cmap="Blues"))
+        csv = linechart.to_csv(index=False).encode("utf-8")
+        st.download_button('Descargar', data = csv, file_name = "Fallas por Semana.csv", mime ='text/csv',
+                        help = 'Haz click para descargar la información')
+
+
+with col2:
+    filtered_df["Dia"] = filtered_df["FECHA"].dt.to_period("D")
+    st.write('<p style="font-size:25px; font-weight:bold; text-align:center;"> Fallas por Dia</p>', unsafe_allow_html=True)
+
+    linechart = pd.DataFrame(filtered_df.groupby(filtered_df["Dia"].dt.strftime("%D"))["FALLAS"].sum()).reset_index()
+    fig10 = px.line(linechart, x = "Dia", y="FALLAS", labels = {"FALLAS": "Cantidad"},height=500, width = 1000,template="ggplot2")
+    fig10.update_layout({
+    'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+    'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+    fig10.update_traces(marker_color='#FF8000')
+    st.plotly_chart(fig10,use_container_width=True)
+
+    with st.expander("Fallas Por Dia"):
+        st.write(linechart.T.style.background_gradient(cmap="Blues"))
+        csv = linechart.to_csv(index=False).encode("utf-8")
+        st.download_button('Descargar', data = csv, file_name = "Fallas por Dia.csv", mime ='text/csv',
+                        help = 'Haz click para descargar la información')
+
+
+
 
 st.write('<p style="font-size:25px; font-weight:bold; text-align:center;"> Fallas</p>', unsafe_allow_html=True)
 with st.expander("Organización de Fallas"):
@@ -315,8 +361,8 @@ fig1.write_image("reportes/images/fig1.png")
 fig2.write_image("reportes/images/fig2.png")
 fig3.write_image("reportes/images/fig3.png")
 fig4.write_image("reportes/images/fig4.png")
-fig5.write_image("reportes/images/fig5.png")
-fig6.write_image("reportes/images/fig6.png")
+fig9.write_image("reportes/images/fig5.png")
+fig10.write_image("reportes/images/fig6.png")
 fig8.write_image("reportes/images/fig7.png")
 
 
@@ -388,7 +434,7 @@ if export_as_pdf:
     
     
     pdf.ln(70)
-    pdf.cell(190, 10, 'Fallas por Año y Mes',0,0,'L')
+    pdf.cell(190, 10, 'Fallas Semanales y Diarias',0,0,'L')
     pdf.image('reportes/images/fig5.png', 20, 220, 90, 0, 'PNG')
     pdf.image('reportes/images/fig6.png', 105, 220, 90, 0, 'PNG')
     
