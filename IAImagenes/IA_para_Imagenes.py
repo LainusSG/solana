cb75cc1809be4e7a9267cf4859e95a566acb4f53
José Lino santos García <191011022@acatlan.tecnm.mx>
2024-08-27 18:17:08 -0600

    Solana

--- IAImagenes/IA_para_Imagenes.py
+++ IAImagenes/IA_para_Imagenes.py
@@ -4,6 +4,9 @@ from PIL import Image
 import numpy as np
 import datetime
 from sqlalchemy import text
+
+conn = st.connection("postgresql", type="sql")
+
 #st.set_page_config(page_title="Detección calificación de soldadura de Imagen",
 #                   layout="wide",
 #                   page_icon="./imagenes/busquedaIA.jpg")
@@ -27,6 +30,12 @@ with st.spinner('Por favor, espera que tu modelo esta cargando'):
                     data_yaml='./models/data.yml')
     
 
+
+
+
+
+
+
 #Subir imagen
 def subir_imagen():
     archivo_imagen = st.file_uploader(label=":orange[Subir Imagen]")
@@ -48,104 +57,123 @@ def subir_imagen():
             st.error('Tipo de archivo de imagen inválido')
             st.error('Subir solamente png, jpg, jpeg')
             return None
-valorfalla=[]
-object = subir_imagen()
-if object:
-    prediccion = False
-    imagen_obj = Image.open(object['archivo'])
 
-    col1, col2 = st.columns(2)
+def capturar_imagen():
+    object = subir_imagen()
+    if object:
+        imagen_obj = Image.open(object['archivo'])
 
-    with col1:
-        st.info('Vista previa de la imagen')
-        st.image(imagen_obj)
-    with col2:
-        st.subheader('Checa abajo los detalles del archivo')
-        st.json(object['detalles'])
-        boton = st.button('Realizar revisión con la IA')
-        if boton:
-            with st.spinner("""
-                    Detectando fallas de soldadura en la imagen. Por favor, paciencia
-                            """):
-                st.write('Pushaste el botón')
-                arreglo_imagen = np.array(imagen_obj)
-                [pred_img, valorfalla] = yolo.predicciones(arreglo_imagen)
-                pred_img_obj = Image.fromarray(pred_img)
-                prediccion = True
+        col1, col2 = st.columns(2)
+
+        with col1:
+            st.info('Vista previa de la imagen')
+            st.image(imagen_obj)
+        with col2:
+            st.subheader('Checa abajo los detalles del archivo')
+            st.json(object['detalles'])
+        return(imagen_obj)
+    else:
+        return None
+
+def analisis_IA(imagen_obj):
+    valorfalla=[]
+    prediccion=False
+    with st.spinner("""
+            Detectando fallas de soldadura en la imagen. Por favor, paciencia
+                    """):
+        st.write('Se Generó Un Análisis')
+        arreglo_imagen = np.array(imagen_obj)
+        [pred_img, valorfalla] = yolo.predicciones(arreglo_imagen)
+        pred_img_obj = Image.fromarray(pred_img)
+        prediccion = True
 
     if prediccion:
         st.subheader(" Imagen analizada")
         st.caption("Detección de objetos con la IA")
         st.image(pred_img_obj)
+    return valorfalla
 
-valor2 =[]
-valor2_f=[]     
-total_fallas = []
-if len(valorfalla) > 0:
-    for k in valorfalla:
-
-        valor1 = k.split(':')
-        try:
-            valor3 = int(valor1[0])
-            valor2.append(valor3)
-            st.write(valor2)
-    
-        except:
-            igual=False
-            for kk in valor2_f:
-                if kk == valor1[0]:
-                    igual=True
-            if not igual:
-                valor2_f.append(valor1[0])
-            st.write(valor2_f)
-    nnn=0
-    x=0
-    for nn in valor2:
-        nnn+=nn
-        x+=1
-    if x > 0:    
-        calificacion = nnn/x
-    else:
-        calificacion = 0
-    if len(valor2_f) > 0:
-        for kkk in valor2_f:
-            total_fallas.append([calificacion, kkk, 1])
-    else:
-        total_fallas.append([calificacion, 'No hay fallas', 0])
-else:
-    total_fallas.append([0, 'No hay fallas', 0])
         
-st.write (total_fallas) 
+
+
+
+def realizar_limpieza(valorfalla):
+    valor2 =[]
+    valor2_f=[]     
+    total_fallas = []
+    if len(valorfalla) > 0:
+        for k in valorfalla:
+
+            valor1 = k.split(':')
+            try:
+                valor3 = float(valor1[0])
+                valor2.append(valor3)
+                st.write(valor2)
+        
+            except:
+                igual=False
+                for kk in valor2_f:
+                    if kk == valor1[0]:
+                        igual=True
+                if not igual:
+                    valor2_f.append(valor1[0])
+                st.write(valor2_f)
+        nnn=0
+        x=0
+        for nn in valor2:
+            nnn+=nn
+            x+=1
+        if x > 0:    
+            calificacion = nnn/x
+        else:
+            calificacion = 0
+        if len(valor2_f) > 0:
+            for kkk in valor2_f:
+                total_fallas.append([calificacion, kkk, 1])
+        else:
+            total_fallas.append([calificacion, 'No hay fallas', 0])
+    else:
+        total_fallas.append([0, 'No hay fallas', 0])
+            
+    st.write (total_fallas) 
+    return(total_fallas)
 
 
 ################################################## Conexion a la base de dato ################################################################################
 
 
-Fecha = st.date_input("Fecha", datetime.datetime.now(), format="DD/MM/YYYY")
+def create_new_form(imagen):
+    with st.form("myform", clear_on_submit=True):
+        Fecha = st.date_input("Fecha", datetime.datetime.now(), format="DD/MM/YYYY")
 
-Obra = st.text_input("Obra", "")
+        Obra = st.text_input("Obra", "")
 
-Cliente = st.text_input("Cliente", "")
+        Cliente = st.text_input("Cliente", "")
 
-Tipo_Pieza = st.text_input("Tipo de Pieza", "")
+        Tipo_Pieza = st.text_input("Tipo de Pieza", "")
 
-Pieza = st.text_input("Pieza", "")
+        Pieza = st.text_input("Pieza", "")
 
-Categoria = st.text_input("Categoría", "")
+        Categoria = st.text_input("Categoría", "")
 
-Tipo_soldadura = st.text_input("Tipo de Soldadura", "")
+        Tipo_soldadura = st.text_input("Tipo de Soldadura", "")
+        submit = st.form_submit_button(label="Submit")
+        if submit:
+             if imagen is not None:
+                valorfalla= analisis_IA(imagen)
+                TotalFallas= realizar_limpieza(valorfalla)
+                with conn.session as s:
+                    for [calificacion1, tipo_fallas1, fallas1] in TotalFallas:
+                        s.execute(text('INSERT INTO soldadura (fecha, calificacion, obra, cliente, tipo_pieza, pieza, categoria, tipo_soldadura, tipo_fallas, fallas) VALUES (:fecha, :calificacion, :obra, :cliente, :tipo_pieza, :pieza, :categoria, :tipo_soldadura, :tipo_fallas, :fallas );'),
+                        params=dict(fecha=Fecha, calificacion =calificacion1, obra=Obra, cliente=Cliente,  tipo_pieza = Tipo_Pieza, pieza=Pieza, categoria=Categoria, tipo_soldadura=Tipo_soldadura, tipo_fallas=tipo_fallas1, fallas= fallas1 )
+                                    )
+                    
+                        s.commit()
 
-conn = st.connection("postgresql", type="sql")
-for [calificacion1, tipo_fallas1, fallas1] in total_fallas:
-    if st.button("summit"):
-        with conn.session as s:
-        # soldadura = {Fecha, Obra, Cliente, Tipo_Pieza, Pieza, Categoria, Tipo_soldadura}
+                
 
-
-            s.execute(text('INSERT INTO soldadura (fecha, calificacion, obra, cliente, tipo_pieza, pieza, categoria, tipo_soldadura, tipo_fallas, fallas) VALUES (:fecha, :calificacion, :obra, :cliente, :tipo_pieza, :pieza, :categoria, :tipo_soldadura, :tipo_fallas, :fallas );'),
-                params=dict(fecha=Fecha, calificacion =calificacion1, obra=Obra, cliente=Cliente,  tipo_pieza = Tipo_Pieza, pieza=Pieza, categoria=Categoria, tipo_soldadura=Tipo_soldadura, tipo_fallas=tipo_fallas1, fallas= fallas1 )
-                )
-            s.commit()
+imagen=capturar_imagen()
+create_new_form(imagen)
 
 
 
